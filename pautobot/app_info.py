2a83d19d00f2d7c1841e53e0852140cc2a2226de
Viet Anh Nguyen <vietanh.dev@gmail.com>
2023-06-07 01:22:58 +0700

    Bump to v0.0.16

--- pautobot/app_info.py
+++ pautobot/app_info.py
@@ -4,7 +4,7 @@ __appname__ = "PautoBot"
 __description__ = (
     "Private AutoGPT Robot - Your private task assistant with GPT!"
 )
-__version__ = "0.0.15"
+__version__ = "0.0.16"
 
 DATA_ROOT = os.path.abspath(
     os.path.join(os.path.expanduser("~"), "pautobot-data")
