776028b97200e40cb481a843ca911f6027377006
Viet Anh Nguyen <vietanh.dev@gmail.com>
2023-06-11 22:02:40 +0700

    Implement multiple file upload

--- frontend/components/QADBManager.js
+++ frontend/components/QADBManager.js
@@ -12,25 +12,26 @@ import { getBotInfo } from "@/lib/requests/bot";
 
 export default function ModelSelector() {
   const SUPPORTED_FILE_TYPES = [
-    "csv",
-    "docx",
-    "doc",
-    "enex",
-    "eml",
-    "epub",
-    "html",
-    "md",
-    "msg",
-    "odt",
-    "pdf",
-    "pptx",
-    "ppt",
-    "txt",
-    "zip",
+    ".csv",
+    ".docx",
+    ".doc",
+    ".enex",
+    ".eml",
+    ".epub",
+    ".html",
+    ".md",
+    ".msg",
+    ".odt",
+    ".pdf",
+    ".pptx",
+    ".ppt",
+    ".txt",
+    ".zip",
   ];
 
-  const [uploading, setUploading] = useState(false);
   const fileInput = useRef(null);
+  const [dragging, setDragging] = useState(false);
+  const [uploading, setUploading] = useState(false);
   const [documents, setDocuments] = useState([]);
   const refetchDocuments = (contextId) => {
     getDocuments(contextId)
@@ -65,33 +66,58 @@ export default function ModelSelector() {
     return () => clearInterval(interval);
   }, []);
 
-  const checkAndRejectFile = (file) => {
+  const isValidFile = (file) => {
     let fileExtension = file.name?.split(".")?.pop();
     fileExtension = fileExtension?.toLowerCase();
-    if (!fileExtension || !SUPPORTED_FILE_TYPES.includes(fileExtension)) {
-      toast.error("Unsupported file type!");
-      fileInput.current.value = "";
-      return true;
+    if (!fileExtension || !SUPPORTED_FILE_TYPES.includes("." + fileExtension)) {
+      return false;
     }
-    return false;
+    return true;
   };
 
-  const uploadFile = (file) => {
-    checkAndRejectFile(file);
+  const uploadFiles = async (files) => {
+    if (!files || files.length == 0) {
+      toast.error("No file selected.");
+      return;
+    }
+
+    // Clone the files array
+    files = [...files];
+
+    // Start uploading
     setUploading(true);
-    uploadDocument(0, file)
-      .then(async (response) => {
-        toast.success(
-          "File uploaded! Please wait for the data to be ingested."
+
+    let numUploaded = 0;
+    let numFailed = 0;
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      if (!isValidFile(file)) {
+        toast.error(
+          "File type not supported: " + file.name
         );
-        setUploading(false);
-        refetchDocuments(0);
-      })
-      .catch((error) => {
-        toast.error(error);
-        setUploading(false);
-        refetchDocuments(0);
-      });
+        numFailed++;
+        continue;
+      }
+      await uploadDocument(0, file)
+        .then(async (response) => {
+          numUploaded++;
+          refetchDocuments(0);
+        })
+        .catch((error) => {
+          toast.error(error);
+          numFailed++;
+          refetchDocuments(0);
+        });
+    }
+
+    toast.info(
+      "Uploaded " +
+        numUploaded +
+        " file(s). " +
+        (numFailed > 0 ? "Failed to upload " + numFailed + " file(s)." : "")
+    );
+    fileInput.current.value = "";
+    setUploading(false);
   };
 
   return (
@@ -106,7 +132,42 @@ export default function ModelSelector() {
           </span>
         )}
       </div>
-      <div className="relative overflow-x-auto sm:rounded-lg max-h-[200px] overflow-auto mb-4">
+      <div className="relative rounded-lg overflow-hidden max-h-[200px] mb-4"
+        onDragOver={
+          (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+          }
+        }
+        onDragEnter={
+          (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            setDragging(true);
+          }
+        }
+        onDragLeave={
+          (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            setDragging(false);
+          }
+        }
+        onDrop={
+          (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            uploadFiles(e.dataTransfer.files);
+          }
+        }
+      >
+        {dragging && (
+          <div className="absolute inset-0 flex flex-col items-center justify-center bg-gray-100 dark:bg-gray-800 bg-opacity-50">
+            <div className="text-gray-500 dark:text-gray-400">
+              Drop files to upload
+            </div>
+          </div>
+        )}
         <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
           <tbody>
             {documents.map((document, key) => (
@@ -188,10 +249,10 @@ export default function ModelSelector() {
           className="hidden"
           type="file"
           ref={fileInput}
+          accept={SUPPORTED_FILE_TYPES.join(",")}
+          multiple={true}
           onChange={(e) => {
-            const file = e.target.files[0];
-            uploadFile(file);
-            fileInput.current.value = null;
+            uploadFiles(e.target.files);
           }}
         />
         <button
@@ -204,7 +265,7 @@ export default function ModelSelector() {
             fileInput.current.click();
           }}
         >
-          Upload File
+          Upload Files
           {uploading ? <LoadingIcon /> : null}
         </button>
       </div>
