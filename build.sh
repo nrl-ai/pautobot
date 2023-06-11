f51d8c9e6ca00cd8a8941eb38c4ec69a9c3c7182
Joseph Mearman <joseph@mearman.co.uk>
2023-06-11 11:19:28 +0100

    Make build script more robust

--- build.sh
+++ build.sh
@@ -1,10 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 set -e
-rm -rf pautobot/frontend-dist
-rm -rf build
-rm -rf dist
-rm -rf pautobot.egg-info
-rm -rf frontend/.next
-rm -rf frontend/dist
+
+# Remove any existing build artifacts
+rm -rf pautobot/frontend-dist || true
+rm -rf build || true
+rm -rf dist || true
+rm -rf pautobot.egg-info || true
+rm -rf frontend/.next || true
+rm -rf frontend/dist || true
+
+# Build the frontend
 bash build_frontend.sh
+
+# Build the package
 python -m build --sdist --wheel .
